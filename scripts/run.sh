f4ee9142371c6230b70c4d5cb324e85a285a652c
bpinsard <basile.pinsard@gmail.com>
2026-09-23 12:40:10 -0400

    configure container

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apptainer exec -e \
+  -B /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+  -B /tmp/.X11-unix:/tmp/.X11-unix \
+  -B ${XAUTHORITY}:${XAUTHORITY} \
+  --env XAUTHORITY=$XAUTHORITY \
+  --env DISPLAY=$DISPLAY \
+  --env PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+  $@
